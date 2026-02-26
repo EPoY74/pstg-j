b4311cb8137b604b00d5e9288a0f0615073692c3
EvgeniiPetrovIceBookWin <epoy74@gmail.com>
2026-02-26 05:46:56 +0400

    chore(dev): Initial commit. Read data from holding registers

--- src/main/java/evgeniipetrov/App.java
+++ src/main/java/evgeniipetrov/App.java
@@ -1,6 +1,12 @@
 package evgeniipetrov;
 
+import com.digitalpetri.modbus.Modbus;
+import com.digitalpetri.modbus.tcp.Netty;
 import com.digitalpetri.modbus.exceptions.ModbusExecutionException;
+import com.digitalpetri.modbus.exceptions.ModbusResponseException;
+import com.digitalpetri.modbus.exceptions.ModbusTimeoutException;
+import com.digitalpetri.modbus.pdu.ReadHoldingRegistersRequest;
+import com.digitalpetri.modbus.pdu.ReadHoldingRegistersResponse;
 import com.digitalpetri.modbus.tcp.client.NettyTcpClientTransport;
 import com.digitalpetri.modbus.client.ModbusTcpClient;
 
@@ -22,6 +28,30 @@ public class App {
 
         try {
             client.connect();
+            while(true) {
+                try {
+                    ReadHoldingRegistersResponse response = client.readHoldingRegisters(
+                            1,
+                            new ReadHoldingRegistersRequest(0, 12)
+                    );
+                    System.out.println("Response: " + response);
+                    Thread.sleep(500);
+                }
+                catch (ModbusExecutionException err) {
+                    System.err.println("ModbusTcpClientTransport (execution) failed to read holding registers: " + err.getMessage());
+                }
+                catch (ModbusTimeoutException err) {
+                    System.out.println("ModbusTcpClientTransport failed to read holding registers: " + err.getMessage());
+                }
+                catch (ModbusResponseException err) {
+                    System.err.println("ModbusTcpClientTransport (response) failed to read holding registers: " + err.getMessage());
+                }
+                catch (InterruptedException err){
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+
         }
         catch (ModbusExecutionException err){
             System.err.println("ModbusTcpClientTransport failed to connect: " + err.getMessage());
@@ -33,9 +63,9 @@ public class App {
 
             catch (ModbusExecutionException err){
                 System.err.println("ModbusTcpClientTransport failed to disconnect: " + err.getMessage());}
+            Modbus.releaseSharedResources();
+            Netty.releaseSharedResources();
         }
-
-
 
     }
 }
